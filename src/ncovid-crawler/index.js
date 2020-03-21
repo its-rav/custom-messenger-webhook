647b2039d8044b2f60478ff78fb538a81edf3ea5
NhanVo <voasd123@gmail.com>
2020-03-21 17:56:06 +0700

    Add reject unauthorized

--- src/ncovid-crawler/index.js
+++ src/ncovid-crawler/index.js
@@ -6,7 +6,9 @@ const url = "https://ncov.moh.gov.vn/";
 const fetchData = async () => {
     console.log("Crawling data...")
     // make http call to url
-    let response = await axios(url).catch((err) => console.log(err));
+    let response = await axios(url, {
+        rejectUnauthorized: false
+    }).catch((err) => console.log(err));
 
     if (response.status !== 200) {
         console.log("Error occurred while fetching data");
